6253da3188fb5b0a6ee02ee465c3af02f003fd66
BssPlayer <serge.bss2001@gmail.com>
2020-06-27 12:41:13 +0300

    Update clickscripts.js

--- clickscripts.js
+++ clickscripts.js
@@ -2,6 +2,6 @@ let CP=0;
 function clickfunc() 
 {
       CP=CP+1;
-      document.getElementById("clickpoint").innerHTML = "У тебя " + CP + " ClickPoints;
+      document.getElementById("clickpoint").innerHTML = "У тебя " + CP + " ClickPoints";
 }
 
