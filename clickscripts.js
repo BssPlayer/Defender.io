ba583b709ee22dbcae62737e08f51382923f8745
BssPlayer <serge.bss2001@gmail.com>
2020-06-27 11:46:14 +0300

    Update clickscripts.js

--- clickscripts.js
+++ clickscripts.js
@@ -3,4 +3,8 @@ function clickfunc()
 {
       document.getElementById("clickpoint").innerHTML = "Ты кликнул!";
 }
+document.getElementById("click").onclick = function () {
+      document.getElementById("clickpoint").innerHTML = "Ты кликнул!"
+}
+      
 
