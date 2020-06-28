e3e25a5ae04ca1278915d27a5debfa1b24434c38
BssPlayer <serge.bss2001@gmail.com>
2020-06-28 19:00:09 +0300

    Add files via upload

--- Swarm_scripts.js
+++ Swarm_scripts.js
@@ -22,12 +22,36 @@ function buySwarm(tier)
             player.swarm2factor *= player.factorPerTen;
         }
     }
+    if (tier==3 && player.meat >= player.swarm3cost)
+    {
+        player.meat -= player.swarm3cost;
+        player.swarm3boughtCount++;
+        player.swarm3count++;
+        if (player.swarm3boughtCount % 10 == 0)
+        {
+            player.swarm3cost *= player.swarm3costUp; 
+            player.swarm3factor *= player.factorPerTen;
+        }
+    }
+    if (tier==4 && player.meat >= player.swarm4cost)
+    {
+        player.meat -= player.swarm4cost;
+        player.swarm4boughtCount++;
+        player.swarm4count++;
+        if (player.swarm4boughtCount % 10 == 0)
+        {
+            player.swarm4cost *= player.swarm4costUp; 
+            player.swarm4factor *= player.factorPerTen;
+        }
+    }
 }
 
 setInterval(swarmUpdate, 1000);
 function swarmUpdate()
 {
-    player.meat += player.swarm1count * player.swarm1factor;
-    player.swarm1count += player.swarm2count * player.swarm2factor;
+    player.meat += player.meatPerSec;
+    player.swarm1count += swarm1growth;
+    player.swarm2count += swarm2growth;
+    player.swarm3count += swarm3growth;
 }
 
