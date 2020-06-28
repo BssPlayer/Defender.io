72330c365df983252562d7dd4c9953907c1d6a27
BssPlayer <serge.bss2001@gmail.com>
2020-06-28 17:11:06 +0300

    Add files via upload

--- Game.js
+++ Game.js
@@ -21,8 +21,8 @@ function gameUpdate()
     document.getElementById('swarm1').textContent = '1st swarm: Drone x' + player.swarm1factor;
     document.getElementById('swarm2').textContent = '2nd swarm: Queen x' + player.swarm2factor;
     
-    document.getElementById("swarm1count").textContent = player.swarm1count + ' (' + player.swarm1count % 10 + ')';
-    document.getElementById("swarm2count").textContent = player.swarm2count + ' (' + player.swarm2count % 10 + ')';
+    document.getElementById("swarm1count").textContent = player.swarm1count + ' (' + player.swarm1boughtCount % 10 + ')';
+    document.getElementById("swarm2count").textContent = player.swarm2count + ' (' + player.swarm2boughtCount % 10 + ')';
 
     document.getElementById('swarm1Max').textContent = 'Cost: ' + player.swarm1cost + " meat";
     document.getElementById('swarm2Max').textContent = 'Cost: ' + player.swarm2cost + " meat";
